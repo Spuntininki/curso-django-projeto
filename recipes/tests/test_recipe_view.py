c411464082dec387064a3c793c985fb87cf3eaf9
Spuntininki <lucasfmoreira7@gmail.com>
2022-10-02 19:45:56 -0300

    improved recipe view test organization

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -6,8 +6,7 @@ from recipes import views
 from .test_recipe_base import RecipeBaseTest
 
 
-class RecipeViewTest(RecipeBaseTest):
-
+class RecipeHomeViewTest(RecipeBaseTest):
     def test_if_home_view_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -25,7 +24,18 @@ class RecipeViewTest(RecipeBaseTest):
         self.assertIn('<h1>No recipes found here 🥲</h1>',
                       response.content.decode('utf-8'))
 
-# @skip"WIP" significa "WORK IN PROGRESS"
+    def test_if_recipe_home_is_published_equal_False_is_working(self):
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
+
+    # @skip"WIP" significa "WORK IN PROGRESS"
+
     def test_if_home_template_loads_recipes(self):
         self.make_recipe(preparation_time=5)
         response = self.client.get(reverse('recipes:home'))
@@ -35,6 +45,17 @@ class RecipeViewTest(RecipeBaseTest):
         self.assertIn('Recipe Title', response_content_recipe)
         self.assertIn('5 Minutos', response_content_recipe)
         self.assertIn('5 Porções', response_content_recipe)
+
+
+class RecipeCategoryViewTest(RecipeBaseTest):
+
+    def test_if_recipe_category_is_published_equal_False_is_working(self):
+        recipe = self.make_recipe(is_published=False)
+
+        respose = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': recipe.category.id}))
+
+        self.assertEqual(respose.status_code, 404)
 
     def test_if_category_template_loads_recipes(self):
         required_title = 'This title is for a template test'
@@ -54,17 +75,28 @@ class RecipeViewTest(RecipeBaseTest):
         )
         self.assertIs(view.func, views.category)
 
-    def test_if_detail_page_recipe_is_correct(self):
-        view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
-        )
-        self.assertIs(view.func, views.recipe)
-
     def test_if_recipe_category_returns_404_when_no_category(self):
         response = self.client.get(
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+
+class RecipeDetailViewTest(RecipeBaseTest):
+
+    def test_if_view_recipes_is_published_equal_False_is_working(self):
+        recipe = self.make_recipe(is_published=False)
+
+        respose = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        self.assertEqual(respose.status_code, 404)
+
+    def test_if_detail_page_recipe_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipe)
 
     def test_if_recipes_recipe_returns_404_when_no_recipe(self):
         response = self.client.get(
